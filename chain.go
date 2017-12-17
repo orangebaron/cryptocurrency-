0298607b4627329f3836e955f201ad272ef5a413
orangebaron <orangebaronroblox@gmail.com>
2017-12-16 23:34:10 -0500

    most goddamn fun i've had in my life

--- chain.go
+++ chain.go
@@ -3,17 +3,32 @@ package cryptocurrency
 import (
 	"crypto/ecdsa"
 	"crypto/elliptic"
+	"math/big"
 )
+
+//R is the R value used for signatures.
+var R = big.NewInt(65)
+
+//S is the S value used for signatures.
+var S = big.NewInt(65)
+
+//Signature is a cryptographic signature.
+type Signature []byte
 
 //Curve is the curve used in signatures.
 var Curve = elliptic.P521()
 
 //Transaction is a transfer of coins from one user to another.
 type Transaction struct {
-	Inputs           []*Transaction
-	Output           *ecdsa.PublicKey
-	CoinsTransferred uint64
-	Signatures       [][]byte
+	Inputs []struct {
+		Input     *Transaction
+		OutputNum uint8
+	}
+	Outputs []struct {
+		Key              *ecdsa.PublicKey
+		CoinsTransferred uint64
+	}
+	Signatures []Signature
 }
 
 //Block is a group of transactions in the blockchain.
--- chainfuncs.go
+++ chainfuncs.go
@@ -3,40 +3,158 @@ package cryptocurrency
 import (
 	"crypto/ecdsa"
 	"crypto/sha256"
-	"encoding/json"
+	"encoding/json" //TODO: don't use json
 )
+
+//TODO: all of the maps here are pointer maps, so clear them when you're done
 
 //BytesObject can be converted to a byte slice.
 type BytesObject interface {
 	GetBytes() []byte
 }
 
+var generatedBytes map[interface{}][]byte
+
+//GetBytes converts a transaction into bytes.
+func (t *Transaction) GetBytes() []byte {
+	bytes, checked := generatedBytes[t]
+	if checked {
+		return bytes
+	}
+
+	marshalBlock := struct {
+		Inputs []struct {
+			Input     []byte
+			OutputNum uint8
+		}
+		Outputs []struct {
+			Key              *ecdsa.PublicKey
+			CoinsTransferred uint64
+		}
+	}{
+		make([]struct {
+			Input     []byte
+			OutputNum uint8
+		}, len(t.Inputs)),
+		t.Outputs,
+	}
+	for i, inp := range t.Inputs {
+		marshalBlock.Inputs[i].Input = inp.Input.GetBytes()
+		marshalBlock.Inputs[i].OutputNum = inp.OutputNum
+	}
+	bytes, _ = json.Marshal(marshalBlock)
+	generatedBytes[t] = bytes
+	return bytes
+}
+
 //GetBytes converts a block into bytes.
-func (b *Block) GetBytes() []byte { //TODO: don't use json
+func (b *Block) GetBytes() []byte {
+	bytes, checked := generatedBytes[b]
+	if checked {
+		return bytes
+	}
+
 	marshalBlock := struct {
 		PrevHash     []byte
 		Transactions []Transaction
 		Miner        *ecdsa.PublicKey
 		Nonce        uint64
 	}{GetHash(b.PrevBlock), b.Transactions, b.Miner, b.Nonce}
-	bytes, _ := json.Marshal(marshalBlock)
+	bytes, _ = json.Marshal(marshalBlock)
+	generatedBytes[b] = bytes
 	return bytes
 }
 
+var hashes map[*BytesObject][]byte
+
 //GetHash returns a block's hash.
 func GetHash(obj BytesObject) []byte {
+	_, checked := hashes[&obj]
+	if checked {
+		return hashes[&obj]
+	}
 	hash := sha256.New()
 	hash.Write(obj.GetBytes())
-	return hash.Sum(nil)
+	sum := hash.Sum(nil)
+	hashes[&obj] = sum
+	return sum
 }
 
-var validityChecked map[*Block]bool
-var validity map[*Block]bool
+var validities map[interface{}]bool
+var spent map[*Transaction]*Transaction
+
+//IsValid checks a signature's validity.
+func (s Signature) IsValid(signed BytesObject, signer *ecdsa.PublicKey) bool {
+	return ecdsa.Verify(signer, signed.GetBytes(), R, S)
+}
+
+//IsValid checks a transaction's validity.
+func (t *Transaction) IsValid() bool {
+	v, checked := validities[t]
+	if checked {
+		return v
+	}
+	validities[t] = false
+
+	var inpMoney, otpMoney uint64
+	for _, inp := range t.Inputs {
+		if !inp.Input.IsValid() {
+			return false
+		}
+		if spent[inp.Input] != nil && spent[inp.Input] != t {
+			return false
+		}
+		inpMoney += inp.Input.Outputs[inp.OutputNum].CoinsTransferred
+	}
+	for _, otp := range t.Outputs {
+		otpMoney += otp.CoinsTransferred
+	}
+	if inpMoney != otpMoney {
+		return false
+	}
+
+	if len(t.Inputs) != len(t.Signatures) {
+		return false
+	}
+	for i, sig := range t.Signatures {
+		if !sig.IsValid(t, t.Inputs[i].Input.Outputs[t.Inputs[i].OutputNum].Key) {
+			return false
+		}
+	}
+
+	validities[t] = true
+	return true
+}
+func (t *Transaction) markInputsAsSpent() {
+	for _, inp := range t.Inputs {
+		spent[inp.Input] = t
+	}
+}
+func (t *Transaction) markInputsAsUnspent() {
+	for _, inp := range t.Inputs {
+		spent[inp.Input] = nil
+	}
+}
 
 //IsValid checks a block's validity.
 func (b *Block) IsValid() bool {
-	if validityChecked[b] {
-		return validity[b]
+	v, checked := validities[b]
+	if checked {
+		return v
 	}
-	return true //TODO: yES,
+	validities[b] = false
+	if !b.PrevBlock.IsValid() {
+		return false
+	}
+	for _, t := range b.Transactions {
+		if !t.IsValid() {
+			return false
+		}
+	}
+
+	for _, t := range b.Transactions {
+		t.markInputsAsSpent()
+	}
+	validities[b] = true
+	return true
 }
